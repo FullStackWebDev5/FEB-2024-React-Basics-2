5c3f2efe05828ae77e6096dd2b700cd351731599
AnkitKumarMishra5 <ankit5kumar14@gmail.com>
2024-05-03 22:40:06 +0530

    React.js: Asynchronous State Updates

--- src/components/Count.jsx
+++ src/components/Count.jsx
@@ -8,12 +8,24 @@ function Count() {
     setCount(count + 1)
   }
 
+  const increment5 = () => {
+    setCount((prevCount) => prevCount + 1)
+    setCount((prevCount) => prevCount + 1)
+    setCount((prevCount) => prevCount + 1)
+    setCount((prevCount) => prevCount + 1)
+    setCount((prevCount) => prevCount + 1)
+  }
+
   return (
     <div>
       <p style={{ fontSize: '5em' }}>{count}</p>
       <button onClick={increment}>Increment</button>
+      <button onClick={increment5}>Increment 5</button>
     </div>
   )
 }
 
 export default Count
+
+
+// State Handlers -> Asynchronous in nature
